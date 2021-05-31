cb866c06edb1c7247af6b0ff673af7df7b7fad6b
DESKTOP-QT2040F\LYC5254 <1205504983@qq.com>
2021-05-31 21:13:03 +0800

    使用nodejs制作带表单的页面

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write("<head><title>xiake</title></head>");
+  res.write('<body>');
+  res.write('<form method="post">');
+  res.write('username:<input name="username"></br>');
+  res.write('password:<input name="password" type="password"></br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
