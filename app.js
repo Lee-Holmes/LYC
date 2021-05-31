3ce3e5301320ca2c6a44b2237495148774e093e8
DESKTOP-QT2040F\LYC5254 <1205504983@qq.com>
2021-05-31 21:30:38 +0800

    no message

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  if(req.method == "GET"){
+	doGet(req,res);
+	res.end();
+  }else if(req.method == "POST"){
+	doPost(req,res);
+	res.end();
+  }else{
+	res.end();
+  }
+});
+
+function doGet(req,res){
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
@@ -17,8 +29,12 @@ const server = http.createServer((req, res) => {
   res.write('</body>');
   res.write('</html>');
   res.end();
-});
+}
 
+
+function doPost(req,res){
+	res.write("successed!");
+}
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
