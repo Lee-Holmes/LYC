c8d880ad408e3d2c152a7d9bc8e98106f4b5df26
DESKTOP-QT2040F\LYC5254 <1205504983@qq.com>
2021-06-01 14:06:05 +0800

    get与post请求解析

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require('http');
-
+var url = require('url');
+var qs = require('querystring');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  if(req.method == "GET"){
+    doGet(req,res);
+    res.end();
+  }else if(req.method == "POST"){
+    doPost(req,res);
+    res.end();
+  }else{
+    res.end();
+  }
+})
+
+function doGet(req,res) {
+  var obj=qs.parse(url.parse(req.url).query);
+  console.log(obj);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
@@ -19,9 +34,17 @@ const server = http.createServer((req, res) => {
   res.end();
 }
 
-
 function doPost(req,res){
-	res.write("successed!");
+  var formData='';
+  req.on('data',function (data){
+    formData+=data;
+
+  });
+  req.on('end',function () {
+    var obj=qs.parse(formData);
+    console.log(obj);
+    res.end();
+  })
 }
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
